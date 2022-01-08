655e0502c3a046dae819b0d429f3c2b93e341927
Aman Sonwani <amansonwani43@gmail.com>
2022-01-08 11:14:27 +0530

    converting all components over to styled-components

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
-import '../sign-in-and-sign-up/sign-in-and-sign-up.styles.scss';
 import SignUp from "../../components/sign-up/sign-up.component";
 
-const SignInSignUpPage = () =>(
-    <div className="sign-in-and-sign-up">
-        <SignIn/>
-        <SignUp/>
-    </div>
-)
+import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
 
-export default SignInSignUpPage;
+const SignInAndSignUpPage = () => (
+  <SignInAndSignUpContainer>
+    <SignIn />
+    <SignUp />
+  </SignInAndSignUpContainer>
+);
+
+export default SignInAndSignUpPage;
