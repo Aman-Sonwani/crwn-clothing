15fc396f10e977d39db1f402f8cea40dd0ee829e
Aman Sonwani <amansonwani43@gmail.com>
2022-01-16 21:05:08 +0530

    creating a redux-saga for fetching our collections from the firestore database

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,38 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
+import { fetchCollectionsFaliure, fetchCollectionsSuccess } from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync(){
+    yield console.log('I am fired');
+
+    try {
+        const collectionRef =firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(
+            convertCollectionsSnapshotToMap,
+            snapshot
+        );
+        yield put(fetchCollectionsSuccess(collectionsMap));  
+    }catch (error) {
+        yield put(fetchCollectionsFaliure(error.message));
+    }
+
+    // redux-thunk
+
+    // const collectionRef = firestore.collection('collections');
+    // dispatch(fetchCollectionsStart());
+
+    // collectionRef.get().then(snapshot => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     dispatch(fetchCollectionsSuccess(collectionsMap));
+    // }).catch(error => dispatch(fetchCollectionsFaliure(error.message)));
+}
+
+export function* fetchCollectionsStart(){
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionsAsync
+    );
+}
